f503bc34c2df1e822afbd429c4d7dc0b0d44ac74
YashithaRavindi <yashitha94@gmail.com>
2021-10-17 01:34:54 +0530

    initial

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const bcrypt = require("bcrypt")
+
+const users = []
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Yashitha'})
@@ -11,8 +15,22 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
+})
+
+app.post('/register', async(req, res) => {
+    try{
+       const hashedPassword = bcrypt.hash(req.body.password, 10)
+    }
+       catch{
+
+       }
+    res.body.email
 })
 
 app.listen(3000)
