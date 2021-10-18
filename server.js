b202c0fac1d20fad7df175a8153fb48c0fca2e34
YashithaRavindi <yashitha94@gmail.com>
2021-10-19 02:18:51 +0530

    mysql integration

--- server.js
+++ server.js
@@ -1,90 +1,125 @@
-if (process.env.NODE_ENV !== 'Production') {
-    require('dotenv').config() 
-}
-
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bcrypt = require('bcrypt')
-const passport = require('passport')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000;
+const sql = require('./sql/sql');
 const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-
-const initializePassport = require('./passport-config')
-initializePassport(
-    passport, 
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
-
-const users = []
-
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+const session = require('express-session');
 app.use(flash())
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    key: 'userId',
+    secret: 'VeryBigSecret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+
 }))
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
+
+app.get('/', (req,res) => {
+    if(req.session.user) {
+        res.render('index', {
+            name: req.session.user[0].name
+        })
+    } else {
+        res.redirect('/Login')
+    }
 })
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
+
+app.get('/Login', (req, res) => {
+    if(!req.session.user) {
+        res.render('login.ejs', {error_msg:""})
+    } else {
+        res.redirect('/')
+    }
 })
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
+app.get('/signup', (req, res) => {
+    if(!req.session.user) {
+        res.render('register',{error_msg: ""})
+    } else {
+        res.redirect('/')
+    }
 })
 
-app.post('/register', checkNotAuthenticated, async(req, res) => {
-    try{
-       const hashedPassword = await bcrypt.hash(req.body.password, 10)
-       users.push({
-           id: Date.now().toString(),
-           name: req.body.name,
-           email: req.body.email,
-           password: hashedPassword
-       })
-       res.redirect('/login')
-    }catch{
-        req.redirect('/regsiter')
-       }
-       console.log(users)
-})
-
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-  }
-    res.redirect('/login')
-}
-
-
-function checkNotAuthenticated(req, res, next){
-   if (req.isAuthenticated()) {
-        return res.redirect('/')
+app.post('/signup', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    sql.query('SELECT * FROM users WHERE email = ?;',[email], (err, result) => {
+      if(err) {
+        res.render("register.ejs", { error_msg: err})
       }
-        next()
-}
+      if(result.length > 0) {
+        res.render("register.ejs", { error_msg: "User Exist Try to Login"})
+      } else {
+        bcrypt.hash(password, 10,(err, data) => {
+          if(err) console.error(err)
+          sql.query(`INSERT INTO users ( name, email , password ) VALUES ( '${name}' , '${email}', '${data}')`, (err) => {
+              if(err) console.log(err)
+          })
+          sql.query('SELECT * FROM users WHERE email = ?;',[email], (err, result) => {
+            if(err) {
+              res.render("register.ejs", { error_msg: err})
+            }
+            if(result.length > 0) {
+              bcrypt.compare(password,result[0].password, (err,response) => {
+                if(response) {
+                  req.session.user = result;
+                  res.redirect('/')
+                } else {
+                  res.render("register.ejs", { error_msg: "Something Went Wrong Try Creating A New Account"})
+                }
+              })
+            } else {
+              res.render("register.ejs", { error_msg: "Something Went Wrong Try Creating A New Account"})
+            }
+          }
+        ) 
+      }) 
+      }
+    }
+  )
+})
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    sql.query('SELECT * FROM users WHERE username = ?;',[email], (err, result) => {
+        if(err) {
+          res.render("login.ejs", { error_msg: err})
+        }
+        if(result.length > 0) {
+          bcrypt.compare(password,result[0].password, (err,response) => {
+            if(response) {
+              req.session.user = result;
+              res.redirect('/')
+            } else {
+              res.render("login.ejs", { error_msg: "Credentials Are Invalid"})
+            }
+          })
+        } else {
+          res.render("login.ejs", { error_msg: "User Does Not Exist"})
+        }
+      }
+    )
+})
 
 
-app.listen(3000)
+app.get('/LogOut', (req, res) => {
+  if(req.session.user) {
+    req.session.destroy(err => {
+      if(err) console.log(err)
+      res.redirect('/Login')
+    });
+  } else {
+    res.redirect('/Login')
+  }
+})
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
---
+++ sql/sql.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+const sql = mysql.createConnection({
+  host: "localhost",
+  user: "reader",
+  password: "123456",
+  database: "wiley_library",
+});
+sql.connect((err) => {
+  if (err) console.log(err);
+});
+module.exports=sql
